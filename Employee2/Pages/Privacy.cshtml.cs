c87148b23a1ac8248a1819d6acaaed28a0e8c84f
ArieYativ <135590829+ArieYativ@users.noreply.github.com>
2023-06-29 15:57:50 +0300

    Update Privacy.cshtml.cs

--- Employee2/Pages/Privacy.cshtml.cs
+++ Employee2/Pages/Privacy.cshtml.cs
@@ -1,19 +1,1 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Employee2.Pages
-{
-    public class PrivacyModel : PageModel
-    {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
-    }
-}
